54579a1318b34edc5f2284114faca264da57a32d
tarasowski <dimitri+github@tarasowski.de>
2019-02-02 15:35:43 +0100

    Added alert after the pause

--- timer.js
+++ timer.js
@@ -5,18 +5,21 @@ const compose = (...fns) => x =>
 
 const print = type => sec =>
     process.stdout.write(`Remaining Time for ${type}: ` + Math.round(sec / 60) + " Minutes \r")
-
+	
 const message = msg => () =>
     exec(`osascript -e 'display notification "${msg.message}" with title "${msg.title}" sound name "Submarine"'`)
 
+const messageAlert = msg => () =>
+         exec(`osascript -e 'tell application (path to frontmost application as text) to display dialog "${msg}" buttons {"YES", "NO"} with icon caution sound name "Submarine"'`)
+
 const pause = (sec) =>
     sec === 0
-        ? (message({ title: 'End', message: 'Pause is over!' })(), console.log('\x1b[32m', 'Pause is over!', '\x1b[0m'))
+        ? exec(`osascript -e 'tell application (path to frontmost application as text) to display dialog "Your Pause is over. Please start another Pomodoro sessions!" buttons {"OK"} with icon caution'`) 
         : print('Pause')(sec) && setTimeout(() => pause(sec - 1), 1000)
 
 const stop = sec => compose(
-    message({ title: 'Pause', message: '5 Minutes Pause has been started!' }),
-    pause
+	message({title: 'Pause', message: '5 Minutes Pause has been started'}),
+    	pause
 )(sec)
 
 
